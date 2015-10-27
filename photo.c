fab645e141a9edf5ab2a2dc084ffd129561bcc7f
wzy <wzyforgoo@gmail.com>
2015-10-27 14:18:57 +0800

    improve code structure in photo.c

--- photo.c
+++ photo.c
@@ -1,6 +1,4 @@
 #include"Picture.h"
-//#define white 0xff
-//#define black 0x00
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
@@ -96,13 +94,9 @@ static void printAllWord(int s, int t, int num, const unsigned char *word)
 	}
 }
 
-void CreatPIDPicture(int Data[],int DataNum,int Base,int max)
+static FILE* PictureInit(long int width, long int height)
 {
 	FILE *bmp_file;
-	int i, j;
-	long width = PIDW;  //设置图片宽度
-	long height = PIDH; //设置图片高度
-	long date_size = width*height;
 	BIT_MAP_FILE_HEADER bmp_map_file;
 	BIT_MAP_INFO_HEADER bmp_map_info;
 
@@ -115,11 +109,11 @@ void CreatPIDPicture(int Data[],int DataNum,int Base,int max)
 
 	/*写入图位文件头*/
 	bmp_map_file.bfType = 0x4d42;
-	bmp_map_file.bfSize =\
-		sizeof(BIT_MAP_FILE_HEADER) + sizeof(BIT_MAP_INFO_HEADER) + width * height * sizeof(RGB_QUAD);
+	bmp_map_file.bfSize = \
+		sizeof(BIT_MAP_FILE_HEADER)+sizeof(BIT_MAP_INFO_HEADER)+width * height * sizeof(RGB_QUAD);
 	bmp_map_file.bfReserved1 = 0;
 	bmp_map_file.bfReserved2 = 0;
-	bmp_map_file.bfOffBits = sizeof(BIT_MAP_FILE_HEADER) + sizeof(BIT_MAP_INFO_HEADER);
+	bmp_map_file.bfOffBits = sizeof(BIT_MAP_FILE_HEADER)+sizeof(BIT_MAP_INFO_HEADER);
 	fwrite(&bmp_map_file, sizeof(BIT_MAP_FILE_HEADER), 1, bmp_file);
 
 	/*写入信息头*/
@@ -136,56 +130,69 @@ void CreatPIDPicture(int Data[],int DataNum,int Base,int max)
 	bmp_map_info.biYPelsPerMeter = 0;
 	fwrite(&bmp_map_info, sizeof(BIT_MAP_INFO_HEADER), 1, bmp_file);
 
+	return bmp_file;
+}
+
+void CreatPIDPicture(int Data[],int DataNum,int Base,int max)
+{
+	FILE* bmp_file = NULL;
+	bmp_file = PictureInit(PIDW, PIDH);
+
 	/*给图像分配内存*/
-	if ((bmp_data = (RGB_QUAD*)malloc(width * height * sizeof(RGB_QUAD))) == NULL)
+	if ((bmp_data = (RGB_QUAD*)malloc(PIDW * PIDH * sizeof(RGB_QUAD))) == NULL)
 	{
 		printf("图片申请内存失败！\n");
 		exit(0);
 	}
 
 	/*初始化图像*/
-	for (j = 0; j < height; j++)
+	int i, j;
+	for (j = 0; j < PIDH; j++)
 	{
-		for (i = 0; i < width; i++)
+		for (i = 0; i < PIDW; i++)
 		{
-			bmp_data[j * width + i] = black;
+			bmp_data[j * PIDW + i] = black;
 		}
 	}
 
 	/*处理数据*/
-	double Wstep = width / (double)DataNum;
-	double Hstep = height / (double)max;
+	double Wstep = PIDW / (double)DataNum;
+	double Hstep = PIDH / (double)max;
 
 	/*画出输入值*/
 	Base = (int)(Base*Hstep);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < PIDW; i++)
 	{
-		bmp_data[Base*width + i] = red;
-		bmp_data[(Base + 1)*width + i] = red;
-		bmp_data[(Base - 1)*width + i] = red;
+		bmp_data[Base*PIDW + i] = red;
+		bmp_data[(Base + 1)*PIDW + i] = red;
+		bmp_data[(Base - 1)*PIDW + i] = red;
 	}
 
 	/*描点*/
 	int count;
 	int temp;
-	for (i = 0, count = 0; i < width&&count <= DataNum; count++)
+	for (i = 0, count = 0; i < PIDW&&count <= DataNum; count++)
 	{
 		temp = (int)(Data[count] * Hstep);
-		bmp_data[temp * width + i] = white;
-		bmp_data[(temp + 1) * width + i] = white;
-		bmp_data[(temp + 1) * width + i + 1] = white;
-		bmp_data[temp * width + i + 1] = white;
-		bmp_data[(temp + 2) * width + i] = white;
-		bmp_data[(temp + 2) * width + i + 1] = white;
-		bmp_data[(temp + 2) * width + i + 2] = white;
-		bmp_data[(temp + 1) * width + i + 2] = white;
-		bmp_data[temp * width + i + 2] = white;
+		bmp_data[temp * PIDW + i] = white;
+		bmp_data[(temp + 1) * PIDW + i] = white;
+		bmp_data[(temp + 1) * PIDW + i + 1] = white;
+		bmp_data[temp * PIDW + i + 1] = white;
+		if (DataNum < PIDW / 4)
+		{
+			bmp_data[(temp + 2) * PIDW + i] = white;
+			bmp_data[(temp + 2) * PIDW + i + 1] = white;
+			bmp_data[(temp + 2) * PIDW + i + 2] = white;
+			bmp_data[(temp + 1) * PIDW + i + 2] = white;
+			bmp_data[temp * PIDW + i + 2] = white;
+		}
 		i = (int)(i + Wstep);
 	}
 
-	fwrite(bmp_data, sizeof(RGB_QUAD), date_size, bmp_file);
+	fwrite(bmp_data, sizeof(RGB_QUAD), PIDW*PIDH, bmp_file);
 	printf("BMP图像生成成功\n");
 	fclose(bmp_file);
 	free(bmp_data);
+	bmp_data = NULL;
 	system("start cubs.bmp");
 }
